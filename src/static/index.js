df003e6eaf0580c2041a57e6416474740bd683a7
caosiyuan <caosiyuan@youzan.com>
2021-06-01 23:58:19 +0800

    feat: 路由变换

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,23 @@
-console.log(123);
+
+var menu1 = document.getElementById("menu1");
+var menu2 = document.getElementById("menu2");
+var menu3 = document.getElementById("menu3");
+
+
+menu1.addEventListener('click', function(e) {
+    history.pushState(null, '', 'menu1')
+}, false);
+
+menu2.addEventListener('click', function(e) {
+    history.pushState(null, '', 'menu2')
+}, false);
+menu3.addEventListener('click', function(e) {
+    history.pushState(null, '', 'menu3')
+}, false);
+
+window.addEventListener('onpopstate', function() {
+    console.log(111)
+})
+window.onpopstate = function(){
+    console.log(213)
+}
